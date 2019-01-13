70a7b66a170ea7a2321a9b536ffa7a830b8aa333
aayushjn2 <aayushjn2@users.noreply.github.com>
2019-01-13 12:07:18 +0530

    Created Dataset.py

---
+++ Dataset.py
@@ -0,0 +1,21 @@
+#Import various modules 
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt  # Matlab-style plotting
+import seaborn as sns
+color = sns.color_palette()
+sns.set_style('darkgrid')
+import warnings
+def ignore_warn(*args, **kwargs):
+    pass
+warnings.warn = ignore_warn #ignore annoying warning (from sklearn and seaborn)
+
+
+from scipy import stats
+from scipy.stats import norm, skew #for some statistics
+
+#import dataset now
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
